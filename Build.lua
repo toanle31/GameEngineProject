b501cbf7c709fa237ae6acc98cacb8f9f7086762
toanl <toanle31@gmail.com>
2025-01-03 18:41:29 -0500

    Initial Project Set up

---
+++ Build.lua
@@ -0,0 +1,17 @@
+-- premake5.lua
+workspace "Game Engine Project"
+   architecture "x64"
+   configurations { "Debug", "Release", "Dist" }
+   startproject "Game"
+
+   -- Workspace-wide build options for MSVC
+   filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+
+OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
+
+group "Engine"
+	include "Engine/Build-Engine.lua"
+group ""
+
+include "Game/Build-Game.lua"
